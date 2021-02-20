bbd9f322bf15270d1ec2cac65d77f5727488bf67
RENZOje <RENZOje@users.noreply.github.com>
2021-02-20 20:40:21 +0200

    fetching data via api

---
+++ base/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    path('', getRoutes, name='routes'),
+    path('products/', getProducts, name='products'),
+    path('products/<str:pk>/', getProduct, name='product'),
+
+]
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Row, Col} from 'react-bootstrap';
 import Product from '../components/Product'
-import products from '../products';
-
+import axios from 'axios'
 
 function HomeScreen() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        async function fetchProducts() {
+            const {data} = await axios.get('/api/products/');
+            setProducts(data)
+        }
+        fetchProducts()
+    }, []);
 
     return (
         <div>
@@ -12,7 +20,7 @@ function HomeScreen() {
             <Row>
                 {products.map(product => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product} />
+                        <Product product={product}/>
                     </Col>
                 ))}
             </Row>

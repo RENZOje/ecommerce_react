a2ab5c1e1c718b39c9e227380d05c5790d9d5df3
RENZOje <RENZOje@users.noreply.github.com>
2021-02-23 16:23:27 +0200

    add to cart func • cart local storage

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,37 @@
+import React, {useEffect} from 'react'
+import {Link} from 'react-router-dom'
+import {useDispatch, useSelector} from 'react-redux';
+import {Row, Col, Image, ListGroup, Button, Card, Form} from 'react-bootstrap'
+import {Message} from '../components/Message'
+import {addToCart} from '../actions/cartActions'
+
+
+function CartScreen({match, location, history}) {
+    const productId = match.params.id
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1
+
+    const dispatch = useDispatch()
+
+    const cart = useSelector(state => state.cart)
+    const {cartItems} = cart
+    console.log('carItems: ', cartItems)
+
+
+    useEffect(() => {
+            if (productId) {
+                dispatch(addToCart(productId, qty))
+            }
+        }, [dispatch, productId, qty]
+    )
+
+
+    return (
+        <div>
+            Cart
+        </div>
+    )
+
+}
+
+
+export default CartScreen
